a9a769c59306dc6a0ff2482cb2aa2803284583ad
Jose Reyes <jo.reyesgon@gmail.com>
2023-02-09 09:23:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import SimpleTimer from './js/components/SimpleTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleTimer />
+    </div>
+  );
+}
+
+export default App;
